c0736a64792a720bcf858b26f0d965e60e0c8ede
Alexandr Samojlov <samojlovalex@gmail.com>
2024-07-29 22:07:36 +0300

    Initial commit

--- app/src/main/java/com/example/samojlov_av_homeworkandroidstudio_number_41/MainActivity.kt
+++ app/src/main/java/com/example/samojlov_av_homeworkandroidstudio_number_41/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.samojlov_av_homeworkandroidstudio_number_41
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.ColorSpace
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.samojlov_av_homeworkandroidstudio_number_4.TimeConvert
+import com.example.samojlov_av_homeworkandroidstudio_number_41.databinding.ActivityMainBinding
 
-@Suppress("IMPLICIT_CAST_TO_ANY")
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var heading1TV: TextView
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var toolbarMain: androidx.appcompat.widget.Toolbar
 
     private lateinit var firstOperandET: EditText
     private lateinit var secondOperandET: EditText
@@ -25,38 +33,73 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var resultTV: TextView
 
-    private lateinit var resetButtonBTN: Button
-    private lateinit var exitButtonBTN: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        heading1TV = findViewById(R.id.heading1TV)
-        firstOperandET = findViewById(R.id.firstOperandET)
-        secondOperandET = findViewById(R.id.secondOperandET)
-        sumButtonBTN = findViewById(R.id.sumButtonBTN)
-        difButtonBTN = findViewById(R.id.difButtonBTN)
-        resultTV = findViewById(R.id.resultTV)
-        resetButtonBTN = findViewById(R.id.resetButtonBTN)
-        exitButtonBTN = findViewById(R.id.exitButtonBTN)
+        init()
+
+        setSupportActionBar(toolbarMain)
+        title = getString(R.string.toolbar_title)
+        toolbarMain.subtitle = getString(R.string.toolbar_subtitle)
+        toolbarMain.setLogo(R.drawable.png_clipart_time_calendar_graphy_computer_icons_time_angle_text)
 
         sumButtonBTN.setOnClickListener(this)
         difButtonBTN.setOnClickListener(this)
-        resetButtonBTN.setOnClickListener(this)
-        exitButtonBTN.setOnClickListener(this)
-
     }
 
-    @SuppressLint("SetTextI18n")
+    private fun init() {
+        toolbarMain = binding.toolbarMain
+        firstOperandET = binding.firstOperandET
+        secondOperandET = binding.secondOperandET
+        sumButtonBTN = binding.sumButtonBTN
+        difButtonBTN = binding.difButtonBTN
+        resultTV = binding.resultTV
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("SetTextI18n", "ShowToast")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.resetMenuMain -> {
+                firstOperandET.text.clear()
+                secondOperandET.text.clear()
+                resultTV.text = "Результат"
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.toast_reset),
+                    Toast.LENGTH_LONG
+                ).show()
+                resultTV.setTextColor(Color.BLACK)
+            }
+
+            R.id.exitMenuMain -> {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.toast_exit),
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
     override fun onClick(v: View?) {
 
-        var check = true
 
         if (firstOperandET.text.isEmpty() || secondOperandET.text.isEmpty()) {
             return
@@ -67,25 +110,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val timeConvert = TimeConvert()
 
         val result = when (v?.id) {
-            R.id.sumButtonBTN -> timeConvert.timeStructure(
-                timeConvert.timeSecond(first) + timeConvert.timeSecond(
-                    second
+            R.id.sumButtonBTN ->
+                timeConvert.timeStructure(
+                    timeConvert.timeSecond(first) + timeConvert.timeSecond(
+                        second
+                    )
                 )
-            )
 
-            R.id.difButtonBTN -> timeConvert.timeStructure(
-                timeConvert.timeSecond(first) - timeConvert.timeSecond(
-                    second
+
+            R.id.difButtonBTN ->
+                timeConvert.timeStructure(
+                    timeConvert.timeSecond(first) - timeConvert.timeSecond(
+                        second
+                    )
                 )
-            )
-            R.id.resetButtonBTN -> {
-                firstOperandET.text.clear()
-                secondOperandET.text.clear()
-                check = false
-            }
-            R.id.exitButtonBTN -> finish()
+
             else -> "Результат"
         }
-        if (!check)resultTV.text = "Результат" else resultTV.text = result.toString()
+        if (result == "Результат"){
+            return
+        }else {
+            resultTV.setTextColor(Color.parseColor("#8B0000"))
+            Toast.makeText(
+                applicationContext,
+                "Результат: $result",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        resultTV.text = result
     }
 }
